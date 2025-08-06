b67b1230ba013a256b6f82d674ac1b164ce82669
Avijit-Microsoft <v-aghorui@microsoft.com>
2025-08-06 16:27:30 +0530

    Chathistory API connecting with Fabric SQL db with managed identity

--- src/api/common/database/fabric_sqldb_service.py
+++ src/api/common/database/fabric_sqldb_service.py
@@ -1,15 +1,11 @@
 from datetime import datetime, date
 import struct
 import json
-
-# import pandas as pd
-# from api.models.input_models import ChartFilters
 from common.config.config import Config
 import logging
-from azure.identity.aio import DefaultAzureCredential
+from azure.identity.aio import AzureCliCredential
 import pyodbc
 from typing import Tuple, Any
-
 
 async def get_fabric_db_connection():
     """Get a connection to the SQL database"""
@@ -17,50 +13,42 @@ async def get_fabric_db_connection():
 
     server = config.fabric_sqldb_server
     database = config.fabric_sqldb_database
-    username = config.fabric_sqldb_username
-    password = config.fabric_sqldb_database # Assuming this is a mistake in the original code, it should be `fabric_sqldb_password`
     driver = config.fabric_driver
-    mid_id = config.fabric_mid_id
     fabric_sqldb_connection_string = config.fabric_sqldb_connection_string
     app_env = config.app_env
 
     try:
-        async with DefaultAzureCredential(managed_identity_client_id=mid_id) as credential:
-            token = await credential.get_token("https://database.windows.net/.default")
-            # logging.info("FABRIC-SQL-TOKEN: %s" % token.token)
-            token_bytes = token.token.encode("utf-16-LE")
-            token_struct = struct.pack(
-                f"<I{len(token_bytes)}s",
-                len(token_bytes),
-                token_bytes
-            )
-            # Format token for ODBC: interleave nulls and prefix length
-            # exptoken = ''.join([c + '\x00' for c in token.token])  # little-endian UTF-16-like
-            # token_struct = struct.pack("=I", len(exptoken)) + exptoken.encode("utf-8")
+        # logging.info("FABRIC-SQL-app_env: %s" % app_env)
+        # Set up the connection
+        conn=None
+        connection_string = ""
+        if app_env == 'dev':
+            async with AzureCliCredential() as credential:
+                token = await credential.get_token("https://database.windows.net/.default")
+                # logging.info("FABRIC-SQL-TOKEN: %s" % token.token)
+                token_bytes = token.token.encode("utf-16-LE")
+                token_struct = struct.pack(
+                    f"<I{len(token_bytes)}s",
+                    len(token_bytes),
+                    token_bytes
+                )
+                # Format token for ODBC: interleave nulls and prefix length
+                # exptoken = ''.join([c + '\x00' for c in token.token])  # little-endian UTF-16-like
+                # token_struct = struct.pack("=I", len(exptoken)) + exptoken.encode("utf-8")
 
-            SQL_COPT_SS_ACCESS_TOKEN = 1256
+                SQL_COPT_SS_ACCESS_TOKEN = 1256
+                connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};"  
+                conn = pyodbc.connect( connection_string, attrs_before={SQL_COPT_SS_ACCESS_TOKEN: token_struct})              
+        else:
+            connection_string = fabric_sqldb_connection_string
+            conn = pyodbc.connect(connection_string)
 
-            # Set up the connection
-            connection_string = ""
-            if app_env == 'dev':
-                connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};"
-            else:
-                connection_string = fabric_sqldb_connection_string
-
-            # connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};"
-            conn = pyodbc.connect(
-                connection_string, attrs_before={SQL_COPT_SS_ACCESS_TOKEN: token_struct}
-            )
-
-            logging.info("Connected using Default Azure Credential")
-            return conn
+        # logging.info("FABRIC-SQL-connection_string: %s" % connection_string)        
+        # logging.info("FABRIC-SQL-User: %s" % conn.getinfo(pyodbc.SQL_USER_NAME))
+        logging.info("FABRIC-SQL:Successfully Connected to Fabric SQL Database")
+        return conn
     except pyodbc.Error as e:
-        logging.error("Failed with Default Credential: %s", str(e))
-        conn = pyodbc.connect(
-            f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}",
-            timeout=5)
-
-        logging.info("FABRIC-SQL: Connected using Username & Password")
+        logging.info("FABRIC-SQL:Failed to connect Fabric SQL Database")      
         return conn
 
 async def run_query_and_return_json(sql_query: str):
